19a1cca45f313d56c506c78cfc792cf6431cf8a3
pumpking786 <pramitamatya786@gmail.com>
2024-08-24 20:00:07 +0545

    updated

--- js/custom.js
+++ js/custom.js
@@ -44,3 +44,7 @@ $('.team_carousel').owlCarousel({
         }
     }
 })
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
